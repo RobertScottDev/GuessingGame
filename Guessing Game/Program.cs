9bbf4b598d628a93d25bea262bb0b774af7aa499
Robert <Robert.scott190@gmail.com>
2019-01-16 12:16:30 -0500

    Before Lunch

--- Guessing Game/Program.cs
+++ Guessing Game/Program.cs
@@ -6,8 +6,26 @@ namespace Guessing_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guessing game");
-            Console.WriteLine("Son Long");
+            Console.WriteLine("Welcome To The Guessing Game");
+            Console.WriteLine("Please Guess a number 1-10");
+            string userGuess = Console.ReadLine();
+            Console.WriteLine("You guessed: " + userGuess);
+            if (userGuess.Equals("0"))
+            {
+                Console.WriteLine("You must guess a number between 1-10");
+            }
+            // if they guess 7
+            else if (userGuess.Equals("7"))
+            {
+                //They Win
+                Console.WriteLine("YOU WIN");
+            } else
+            {
+                Console.WriteLine("You Lost");
+            }
+            }
+
+
         }
     }
-}
+
