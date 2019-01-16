db605dd392e2f991c528d75545a9b0e761dd4b34
Robert <Robert.scott190@gmail.com>
2019-01-16 14:11:58 -0500

    2:11 last session

--- Guessing Game/Program.cs
+++ Guessing Game/Program.cs
@@ -7,13 +7,17 @@ namespace Guessing_Game
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Guessing Game");
-            Console.WriteLine("Please Guess a number 1-10");
+            Console.WriteLine("Please Guess a number 1-10 or 0 for help");
             string userGuess = Console.ReadLine();
             int userGuessAsInt = Convert.ToInt32(userGuess);
             Console.WriteLine("You guessed: " + userGuess);
-            if (userGuessAsInt.Equals("0"))
+            if (userGuess.Equals("0"))
             {
-                Console.WriteLine("You must guess a number between 1-10");
+                Console.WriteLine("====INTRUCTIONS====");
+                Console.WriteLine("You must guess a whole number between 1-10.");
+                Console.WriteLine("Submit your answer with 0");
+                userGuess = Console.ReadLine();
+
             }
             // if they guess 7
             else if (userGuess.Equals("7"))
